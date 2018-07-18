8890e1cdb1dd20b891837844aa5e6621cdbc8007
Chris Brody <chris@cbrody.com>
2018-07-18 10:27:14 +0100

    Add readmorejs module and library

--- docroot/sites/all/themes/iied/template.php
+++ docroot/sites/all/themes/iied/template.php
@@ -5,12 +5,12 @@
  * This file is empty by default because the base theme chain (Alpha & Omega) provides
  * all the basic functionality. However, in case you wish to customize the output that Drupal
  * generates through Alpha & Omega this file is a good place to do so.
- * 
+ *
  * Alpha comes with a neat solution for keeping this file as clean as possible while the code
  * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
  * for more information on this topic.
  */
- 
+
  function iied_form_alter(&$form, &$form_state, $form_id) {
   if ($form_id == 'search_block_form') {
     $form['search_block_form']['#title'] = t('Search'); // Change the text on the label element
@@ -28,15 +28,15 @@
     $form['search_block_form']['#attributes']['onblur'] = "if (this.value == '') {this.value = 'Search';}";
     $form['search_block_form']['#attributes']['onfocus'] = "if (this.value == 'Search') {this.value = '';}";
   }
-  
+
   if ($form_id == 'custom-search-blocks-form-1') {
      $form['actions']['submit'] = array('#type' => 'image_button', '#src' => base_path() . path_to_theme() . '/colours/set_' . date('d') % 4 . '/search-button.gif');
   }
-  
+
   if ($form_id == 'user_profile_form') {
     unset($form['contact']);
   }
-} 
+}
 /**
 * Process variables for search-result.tpl.php.
 *
