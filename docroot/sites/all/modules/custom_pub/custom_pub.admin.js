cc06f0b79a5d1bf1fc6c7418d0fa4ecda6e30dc6
Chris Brody <chris.brody@iied.org>
2022-07-13 13:47:10 +0100

    Update all modules to latest versions

--- docroot/sites/all/modules/custom_pub/custom_pub.admin.js
+++ docroot/sites/all/modules/custom_pub/custom_pub.admin.js
@@ -1,14 +1,14 @@
 (function ($) {
   "use strict";
   Drupal.behaviors.custom_pub_admin = {
-    attach: function () {
+    attach: function (context) {
       var toggleButtonLabels = {
         edit: Drupal.t('Edit'),
         close: Drupal.t('Close')
       };
-      $("td.custom_pub-option-edit-cell").html('<a href="#">' + toggleButtonLabels.edit + '</a>').css("text-align", "right");
-      $("tr.custom_pub-form-edit").hide();
-      $("td.custom_pub-option-edit-cell > a").bind('click', function () {
+      $("td.custom_pub-option-edit-cell", context).html('<a href="#">' + toggleButtonLabels.edit + '</a>').css("text-align", "right");
+      $("tr.custom_pub-form-edit", context).hide();
+      $("td.custom_pub-option-edit-cell > a", context).bind('click', function () {
         var $this = $(this);
         var opt = [];
         var txt = $this.text();
@@ -18,11 +18,8 @@
         $this.text(opt[txt]);
         return false;
       });
-      $("th.close-custom-pub-table").html('<a>X</a>')
-      .css('text-align', 'right');
-      $("th.close-custom-pub-table > a")
-      .css('cursor', 'pointer')
-      .live('click', function () {
+      $("th.close-custom-pub-table", context).html('<a>X</a>').css('text-align', 'right');
+      $("th.close-custom-pub-table > a", context).css('cursor', 'pointer').click(function () {
         var $this = $(this);
         var $row = $this.parents("tr.custom_pub-form-edit");
         var $link = $row.prev('tr.custom_pub-option-row').find("td.custom_pub-option-edit-cell > a");
